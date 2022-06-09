661abde178f38c1451a265dc362c78299f8bf2af
Julaluk Paladkhwa <j.paladkhwa@gmail.com>
2022-06-10 01:48:47 +0700

    [Web Scraping Test] Using Puppeteer

--- index.js
+++ index.js
@@ -1,27 +1,40 @@
-const puppeteer = require('puppeteer');
-
+// Web Scraping with puppeteer
+const puppeteer = require("puppeteer");
 const url = "https://codequiz.azurewebsites.net/";
+const key = process.argv[2].toUpperCase();
+if (!key) {
+  throw "Please provide key";
+}
 
-async function run () {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url);
-    console.log(url)
-
-    let urls = await page.evaluate(() => {
-        let results = [];
-        console.log("here")
-        let items = document.querySelectorAll('input');
-        // items.forEach((item) => {
-        //     results.push({
-        //         url:  item.getAttribute('href'),
-        //         text: item.innerText,
-        //     });
-        // });
-        console.log("test",items)
-        return results;
-    })
-    browser.close();
-    return urls;
+async function run() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+  await page.click('input[type="button"]');
+//   await page.screenshot({ path: "screenshot01.png" });
+  console.log("key1", key);
+  const htmlKey = await page.evaluate((key) => {
+    const table = document
+      .getElementsByTagName("tr")[1]
+      .getElementsByTagName("td");
+    console.log("key", key);
+    let response;
+    for (let i = 1; i < table.length; i++) {
+      if (
+        key ===
+        document
+          .getElementsByTagName("tr")
+          [i].getElementsByTagName("td")[0]
+          .getInnerHTML().replace(/\s/g, '')
+      ) {
+        response = document
+          .getElementsByTagName("tr")
+          [i].getElementsByTagName("td")[1]
+          .getInnerHTML();
+      }
+    }
+    return `${response} ${key}`;
+  }, key);
+  browser.close();
 }
 run();
